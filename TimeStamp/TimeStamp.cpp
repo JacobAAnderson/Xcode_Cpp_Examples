84d7825b764ac40572dda303679d1106ba5bdee2
Jacob Anderson <jaanderson@fortlewis.edu>
2020-08-05 20:36:50 -0700

    Initial Commit of Code On branch master Changes to be committed: 	modified:   BoilerPlate.xcodeproj/project.pbxproj 	modified:   BoilerPlate.xcodeproj/xcuserdata/jake.xcuserdatad/xcschemes/xcschememanagement.plist 	modified:   BoilerPlate/main.cpp 	new file:   TimeStamp/TimeStamp.cpp 	new file:   Write_to_file/Write_to_file.cpp

--- TimeStamp/TimeStamp.cpp
+++ TimeStamp/TimeStamp.cpp
@@ -1,14 +1,12 @@
 //
 //  main.cpp
-//  BoilerPlate
+//  TimeStamp
 //
 //  Created by Jacob Anderson on 8/5/20.
 //  Copyright © 2020 Jacob Anderson. All rights reserved.
 //
 
 #include <iostream>
-#include <fstream>
-//#include <stdio.h>
 #include <chrono>
 #include <ctime>
 
@@ -33,25 +31,7 @@ int main(int argc, const char * argv[]) {
     std::cout << "TimeStamp 3: " << result << "\t\t[s]\n";
     std::cout << "\nDate and Time: " << std::asctime(std::localtime(&result)) << "\n\n";
     
-
-    
-    time_t t = time(0);   // get time now
-    struct tm * now = localtime( & t );
-
-    char fileName [80];
-    strftime (fileName,80,"/Users/jake/Desktop/%Y%m%d%H%M%S_output.txt",now);
-
-    std::cout << "\n\nWriting to file:  " << fileName << "\n\n\n";
-            
-    // #include <fstream>
-    std::ofstream myfile;
-    myfile.open (fileName, std::ios_base::app);
-    myfile << "Stuff Goes Here!!";
-    myfile << "\n" << 8.62 << " + " << 35 << " = ?";
-    myfile << std::endl;
-    myfile.close();
-
-    
+ 
     std::cout <<"\n\n\n";
     return 0;
 }
---
+++ Write_to_file/Write_to_file.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Write_to_file
+//
+//  Created by Jacob Anderson on 8/5/20.
+//  Copyright © 2020 Jacob Anderson. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, const char * argv[]) {
+    
+    time_t t = time(0);   // get time now
+    struct tm * now = localtime( & t );
+
+    char fileName [80];
+    strftime (fileName,80,"/Users/jake/Desktop/%Y%m%d%H%M%S_output.txt",now);
+
+    std::cout << "\n\nWriting to file:  " << fileName << "\n\n\n";
+            
+    // #include <fstream>
+    std::ofstream myfile;
+    myfile.open (fileName, std::ios_base::app);
+    myfile << "Stuff Goes Here!!";
+    myfile << "\n" << 8.62 << " + " << 35 << " = ?";
+    myfile << std::endl;
+    myfile.close();
+
+    
+    std::cout <<"\n\n\n";
+    return 0;
+}
